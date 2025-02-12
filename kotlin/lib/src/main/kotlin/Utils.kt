2d0a86deecb3fcf1253c7027eb3dc82f6610746e
Mendy Man <mman@svix.com>
2025-02-12 16:24:30 -0500

    kotlin: Comma separate list/set params in url query

--- kotlin/lib/src/main/kotlin/Utils.kt
+++ kotlin/lib/src/main/kotlin/Utils.kt
@@ -1,17 +1,29 @@
 package com.svix.kotlin
 
+import kotlin.reflect.full.isSubclassOf
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.encodeToJsonElement
 import kotlinx.serialization.json.jsonPrimitive
-import kotlin.reflect.full.isSubclassOf
 
 private fun isEnum(v: Any?): Boolean {
     return v != null && v::class.isSubclassOf(Enum::class)
 }
 
+private fun isList(v: Any?): Boolean {
+    return v != null && v::class.isSubclassOf(List::class)
+}
+
+private fun isSet(v: Any?): Boolean {
+    return v != null && v::class.isSubclassOf(Set::class)
+}
+
 internal fun serializeQueryParam(v: Any): String {
     return if (isEnum(v)) {
         Json.encodeToJsonElement(v).jsonPrimitive.content
+    } else if (isList(v)) {
+        (v as List<*>).joinToString(",")
+    } else if (isSet(v)) {
+        (v as Set<*>).joinToString(",")
     } else {
         v.toString()
     }
